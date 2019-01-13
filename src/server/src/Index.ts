716fe52f4e8154fe205993f97c4bc4fc3e2abfe8
bogdanim36 <bogdanim36@gmail.com>
2019-01-13 11:15:34 +0200

    upgrading

--- src/server/src/Index.ts
+++ src/server/src/Index.ts
@@ -1,16 +1,6 @@
 import {ServerApp} from '@server/app/ServerApp';
-import {UsersServerController} from '@module/users/UsersServerController';
-import {ProjectsServerController} from '@module/projects/ProjectsServerController';
-import {TasksServerController} from '@module/tasks/TasksServerController';
-import {TaskTypesServerController} from '@module/task-types/TaskTypesServerController';
-import {TaskStatusServerController} from '@module/task-status/TaskStatusServerController';
 
 const server = new ServerApp();
 const app = server.getApp();
-const users = new UsersServerController(app, server.store);
-const projects = new ProjectsServerController(app, server.store);
-const tasks = new TasksServerController(app, server.store);
-const taskTypes = new TaskTypesServerController(app, server.store);
-const TaskStatus = new TaskStatusServerController(app, server.store);
 server.start();
 export { app };
